05afe108cb2cc70f8131b11e2728b8142255f136
Jed Iquin <jedikin@gmail.com>
2017-12-03 13:08:26 -0800

    :bug: fix positioning bug in twitter page

--- views/TwitterNews.js
+++ views/TwitterNews.js
@@ -165,18 +165,16 @@ export default class TwitterNews extends Component {
                             {this.renderTweets()}
                         </ScrollView>
                     }
-                    
-
-                    <DropdownAlert
-                    ref={(ref) => this.dropdown = ref}
-                    containerStyle={{
-                        backgroundColor: '#2B73B6'
-                    }}
-                    showCancel={true}
-                    onClose={(data) => this.onClose(data)}
-                    onCancel={(data) => this.onClose(data)}
-                    />
                 </View> 
+                <DropdownAlert
+                ref={(ref) => this.dropdown = ref}
+                containerStyle={{
+                    backgroundColor: '#2B73B6'
+                }}
+                showCancel={true}
+                onClose={(data) => this.onClose(data)}
+                onCancel={(data) => this.onClose(data)}
+                />
             </View>
             
         )
